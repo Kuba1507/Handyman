711ad35727777d51008178bf6ce4d40a502da230
Kuba1507 <kuba.romanski@onet.com.pl>
2023-11-09 12:20:36 +0100

    created html for aboutus section and scripted scroll blocking

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,4 @@
+const body = document.querySelector("body");
 const burgerBtn = document.querySelector(".hamburger");
 const mobileNav = document.querySelector(".mobile-nav");
 const mobileNavItems = document.querySelectorAll(".mobile-nav__item");
@@ -5,11 +6,13 @@ const mobileNavItems = document.querySelectorAll(".mobile-nav__item");
 const handleNav = () => {
 	burgerBtn.classList.toggle("is-active");
 	mobileNav.classList.toggle("show-mobile");
+	body.classList.toggle("disable-scroll");
 };
 
 const closeNav = () => {
 	burgerBtn.classList.remove("is-active");
 	mobileNav.classList.remove("show-mobile");
+	body.classList.remove("disable-scroll");
 };
 
 burgerBtn.addEventListener("click", handleNav);
